700cd9cd377db0b9b0fee67854d02d358c550bab
Benjamin <pro.benjamin.leon@gmail.com>
2026-03-24 10:01:53 +0100

    refactor: rename a and b variables

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ var cmd = require("./commanderConfig");
 // Counter of files downloaded
 var downloadedFileCounter = 0;
 // List of files downloaded
-var imagesList = [];
+var downloadedFileList = [];
 // Download an image
-var downloadPlaceHolder = (a, b) => {
-	
-	var fileStream = fs.createWriteStream(b);
+var downloadPlaceHolder = (url, filePath) => {
+
+	var fileStream = fs.createWriteStream(filePath);
 	var handle = (response) => {
 		response.pipe(fileStream);
 		fileStream.on("finish", () => {
 			fileStream.close(() => {
 				
 				downloadedFileCounter++;
-				file_list.push(b);
+				downloadedFileList.push(filePath);
 				var downloadProgress = Math.ceil((downloadedFileCounter / cmd.number) * 100);
 				rdl.cursorTo(process.stdout, 0);
 				process.stdout.write(
@@ -37,12 +37,12 @@ var downloadPlaceHolder = (a, b) => {
 			console.log("Failed");
 		});
 	};
-	if (a.substring(0, 7) === "http://") {
-		http.get(a, (response) => {
+	if (url.substring(0, 7) === "http://") {
+		http.get(url, (response) => {
 			handle(response);
 		});
 	} else {
-		https.get(a, (response) => {
+		https.get(url, (response) => {
 			handle(response);
 		});
 	}
